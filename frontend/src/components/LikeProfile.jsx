e2644836235710be6f2ce0ce7c9f93eb76ed7415
Mahapara <mahanizamani12345678@gmail.com>
2024-02-24 21:11:02 +0500

    components added ui design work updated

---
+++ frontend/src/components/LikeProfile.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LikeProfile = () => {
+  return (
+    <div>
+      <h1>LikeProfile</h1>
+    </div>
+  );
+};
+
+export default LikeProfile;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import Search from "../components/Search";
+import SortRepos from "../components/SortRepos";
+import ProfileInfo from "../components/ProfileInfo";
+import Repos from "../components/Repos";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>HomePage</h1>
+    <div className="m-4">
+      <Search />
+      <SortRepos />
+      <div className="flex gap-4 flex-col lg:flex-row justify-center item-start">
+        <ProfileInfo />
+        <Repos />
+      </div>
     </div>
   );
 };
