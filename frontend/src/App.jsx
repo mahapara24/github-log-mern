1ea3cd5fca1a620dbe59a62c55b762313dcdb421
Mahapara <mahanizamani12345678@gmail.com>
2024-02-24 19:40:37 +0500

    pages added at the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import {
+  ExplorePage,
+  HomePage,
+  LikesPage,
+  LoginPage,
+  SignUpPage,
+} from "./pages";
+import Sidebar from "./components/Sidebar";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-orange-900 text-4xl">hii</h1>
+    <div className="flex">
+      <Sidebar />
+      <div className="max-w-5xl my-5 text-white mx-auto transition-all duration-300 flex-1">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/explore" element={<ExplorePage />} />
+          <Route path="/likes" element={<LikesPage />} />
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,7 @@
+import ExplorePage from "./ExplorePage";
+import HomePage from "./HomePage";
+import LikesPage from "./LikesPage";
+import LoginPage from "./LoginPage";
+import SignUpPage from "./SignUpPage";
+
+export { HomePage, SignUpPage, LoginPage, LikesPage, ExplorePage };
