e033010c10e29566c1624f75d6185bed09963b01
Mahapara <mahanizamani12345678@gmail.com>
2024-02-25 10:55:21 +0500

    Search funtionality added

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -12,34 +12,51 @@ const HomePage = () => {
   const [loading, setLoading] = useState(false);
   const [sortType, setSortType] = useState("fork");
 
-  const getUserProfileAndRepose = useCallback(async () => {
-    setLoading(true);
-    try {
-      const userResponse = await fetch(
-        "https://api.github.com/users/mahapara24"
-      );
-      const userProfile = await userResponse.json();
-      setUserProfile(userProfile);
+  const getUserProfileAndRepose = useCallback(
+    async (username = "mahapara24") => {
+      setLoading(true);
+      try {
+        const userResponse = await fetch(
+          `https://api.github.com/users/${username}`
+        );
+        const userProfile = await userResponse.json();
+        setUserProfile(userProfile);
 
-      const repoRes = await fetch(userProfile.repos_url);
-      const repos = await repoRes.json();
-      setRepos(repos);
-      console.log("Useprofile:", userProfile);
-      console.log("repos:", repos);
-    } catch (error) {
-      toast.error(error.message);
-    } finally {
-      setLoading(false);
-    }
-  }, []);
+        const repoRes = await fetch(userProfile.repos_url);
+        const repos = await repoRes.json();
+        setRepos(repos);
+        console.log("Useprofile:", userProfile);
+        console.log("repos:", repos);
+        return { userProfile, repos };
+      } catch (error) {
+        toast.error(error.message);
+      } finally {
+        setLoading(false);
+      }
+    },
+    []
+  );
 
   useEffect(() => {
     getUserProfileAndRepose();
   }, [getUserProfileAndRepose]);
 
+  const onSearch = async (e, username) => {
+    e.preventDefault();
+
+    setLoading(true);
+    setRepos([]);
+    setUserProfile(null);
+
+    const { userProfile, repos } = await getUserProfileAndRepose(username);
+    setUserProfile(userProfile);
+    setRepos(repos);
+    setLoading(false);
+  };
+
   return (
     <div className="m-4">
-      <Search />
+      <Search onSearch={onSearch} />
       <SortRepos />
       <div className="flex gap-4 flex-col lg:flex-row justify-center item-start">
         {userProfile && !loading && <ProfileInfo userProfile={userProfile} />}
