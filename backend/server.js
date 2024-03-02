6782be1e4602441bc69bea09113151cfeb45f06e
Mahapara <mahanizamani12345678@gmail.com>
2024-03-02 12:38:18 +0500

    updated

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import "./passport/github.auth.js";
 import userRoutes from "./routes/user.route.js";
 import exploreRoutes from "./routes/explore.route.js";
 import authRoutes from "./routes/auth.route.js";
+import path from "path";
 
 import cors from "cors";
 import connectMongoDB from "./db/connectMongoDB.js";
@@ -13,6 +14,9 @@ import connectMongoDB from "./db/connectMongoDB.js";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+const _dirname = path.resolve();
+
 app.use(
   session({ secret: "keyboard cat", resave: false, saveUninitialized: false })
 );
@@ -23,15 +27,19 @@ app.use(passport.session());
 
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Server is Ready");
-});
+// app.get("/", (req, res) => {
+//   res.send("Server is Ready");
+// });
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/explore", exploreRoutes);
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(_dirname, "frontend", "dist", "index.html"));
+});
 
-app.listen(5000, () => {
-  console.log("Server started on http://localhost:5000/");
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
   connectMongoDB();
 });
